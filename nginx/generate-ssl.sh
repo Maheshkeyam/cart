90ac982983af19754d130ee18941554642ae500c
Mahesh KM <maheshkm@Maheshs-MacBook-Air.local>
2025-08-02 13:53:40 +0530

    changes

--- nginx/generate-ssl.sh
+++ nginx/generate-ssl.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-CERT_DIR="/etc/letsencrypt/live/goldencart.biz"
-DUMMY_KEY="$CERT_DIR/privkey.pem"
-DUMMY_CERT="$CERT_DIR/fullchain.pem"
+
+DUMMY_DIR="/etc/letsencrypt/dummy"
+DUMMY_KEY="$DUMMY_DIR/privkey.pem"
+DUMMY_CERT="$DUMMY_DIR/fullchain.pem"
 
 # Create dummy cert if real one doesn't exist
-if [ ! -f "$DUMMY_CERT" ] || [ ! -f "$DUMMY_KEY" ]; then
-  echo "Generating self-signed dummy SSL certificate for goldencart.biz..."
-  mkdir -p "$CERT_DIR"
 
-  openssl req -x509 -nodes -newkey rsa:2048 \
-    -days 1 \
-    -keyout "$DUMMY_KEY" \
-    -out "$DUMMY_CERT" \
-    -subj "/CN=localhost"
-else
-  echo "SSL certificate already exists. Skipping dummy cert generation."
-fi
+echo "Generating self-signed dummy SSL certificate for goldencart.biz..."
+mkdir -p "$DUMMY_DIR"
+
+openssl req -x509 -nodes -newkey rsa:2048 \
+-days 1 \
+-keyout "$DUMMY_KEY" \
+-out "$DUMMY_CERT" \
+-subj "/CN=localhost"
+
 
 # Start nginx
 echo "Starting Nginx..."
